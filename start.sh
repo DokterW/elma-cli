59cae1d5a75d463664dab2330d3ac8b56a81c819
Dr. Waldijk <DokterW@users.noreply.github.com>
2019-04-09 21:46:09 +0200

    args

--- start.sh
+++ start.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-# elma-cli v0.1
+# elma-cli v0.2
 # Made by Dokter Waldijk
 # Search ELMA in the terminal.
 # By running this script you agree to the license terms.
 # Config ----------------------------------------------------------------------------
 ELMANAM="elma-cli"
-ELMAVER="0.1"
-ELMAQUR=$1
-# ELMAQUR=$(echo "$ELMAQUR" | sed -r 's/ /%20/g')
+ELMAVER="0.2"
+ELMAQUR="$@"
+ELMAQUR=$(echo "$ELMAQUR" | sed -r 's/ /%20/g')
 # -----------------------------------------------------------------------------------
-ELMAAPI=$(curl -s http://hotell.difi.no/api/json/difi/elma/participants?query=$ELMAQUR | sed -r 's/Ja/Yes/g' | sed -r 's/Nei/No/g')
+ELMAAPI=$(curl -s https://hotell.difi.no/api/json/difi/elma/participants?query=$ELMAQUR | sed -r 's/Ja/Yes/g' | sed -r 's/Nei/No/g')
 ELMALIN=$(echo "$ELMAAPI" | jq -r '.entries[].name' | wc -l)
 ELMACNT="0"
 echo "$ELMANAM v$ELMAVER"
-echo ""
 if [[ -n $ELMAQUR ]]; then
-    until [[ "$ELMACNT" = "2" ]]; do
+    until [[ "$ELMACNT" = "$ELMALIN" ]]; do
+        echo ""
         echo -n "   Name: "
         echo -n "$ELMAAPI" | jq -r ".entries[$ELMACNT].name"
         echo -n "    ICD: "
@@ -27,9 +27,6 @@ if [[ -n $ELMAQUR ]]; then
         echo -n "EHF 3.0: "
         echo -n "$ELMAAPI" | jq -r ".entries[$ELMACNT].EHF_INVOICE_CREDITNOTE_2_0"
         ELMACNT=$(expr $ELMACNT + 1)
-        if [[ "$ELMACNT" -ge "1" ]]; then
-            echo ""
-        fi
     done
 else
     echo "$ELMANAM v$ELMAVER"
